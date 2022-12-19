74ee39fb83e97d8ab2873ada7d93a3400f357231
DevashishDas3 <dedas2024@gmail.com>
2022-12-18 22:29:12 -0500

    navbar creation

---
+++ components/Navbar.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+const Nav = styled.nav`
+  height = 80px;
+  background: blue;
+  text-align: center;
+  
+  `
+
+
+const Navbar = () => {
+  return (
+    <Nav>
+        <h1>Navbar</h1>
+    </Nav>
+  );
+};
+
+export default Navbar;
--- pages/people.js
+++ pages/people.js
@@ -1,17 +1,16 @@
-//This file is the home page
 import Head from 'next/head'
 import Image from 'next/image'
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function People() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Natick Commission on Disability</title>
       </Head>
       <Navbar />
-      <h1>Home</h1>
+      <h1>People</h1>
     </div>
   );
 }
