3618ff9991ac69adc7e5c361f29db014e73553db
DevashishDas3 <dedas2024@gmail.com>
2023-01-04 21:25:40 -0500

    Events, Fonts, and Compiler Fix Attempts
    When trying to create boxes for events, compiler did not  allow @next/font. So, I  ran  "npm install @next/font" and that still didnt work. So, I added something in next config to force compile with SWC instead  of babel but that   could cause app crashing so I commented  it.

--- next.config.js
+++ next.config.js
@@ -4,3 +4,17 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+// Below (lines 15-19 Jan 4) will force SWC compiling if uncommented, custom babel does not work with next/@font
+//Warning messages when force compiling with SWC:
+// warn  - You have enabled experimental feature (forceSwcTransforms) in next.config.js.
+// Important: warn  - Experimental features are not covered by semver, and may cause unexpected or broken application behavior. Use at your own risk.
+
+
+/*
+module.exports = {
+  experimental: {
+    forceSwcTransforms: true,
+  },
+}
+*/
--- pages/events.js
+++ pages/events.js
@@ -1,12 +1,67 @@
 import Head from 'next/head';
 import Layout from '../components/Layout';
-import styles from '../styles/main.module.css'
+import styles from '../styles/Events.module.css'
+import { Inter } from '@next/font/google' //dysfunctional with compiler
 
 export default function Events() {
   return <>
-    <Layout title="Natick Commission on Disability">
-      <h1>Events</h1>
-      <h1>HELLO</h1>
-    </Layout>
+<div className={styles.grid}>
+          <a
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={Inter.className}>
+              Docs <span>-&gt;</span>
+            </h2>
+            <p className={Inter.className}>
+              Find in-depth information about Next.js features and&nbsp;API.
+            </p>
+          </a>
+
+          <a
+            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={Inter.className}>
+              Learn <span>-&gt;</span>
+            </h2>
+            <p className={Inter.className}>
+              Learn about Next.js in an Interactive course with&nbsp;quizzes!
+            </p>
+          </a>
+
+          <a
+            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={Inter.className}>
+              Templates <span>-&gt;</span>
+            </h2>
+            <p className={Inter.className}>
+              Discover and deploy boilerplate example Next.js&nbsp;projects.
+            </p>
+          </a>
+
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className={Inter.className}>
+              Deploy <span>-&gt;</span>
+            </h2>
+            <p className={Inter.className}>
+              Instantly deploy your Next.js site to a shareable URL
+              with&nbsp;Vercel.
+            </p>
+          </a>
+        </div>
   </>;
 }
