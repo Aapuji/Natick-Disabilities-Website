b5afa9a3b56de5f868e270600d4a1d2abe5c18de
Aapuji <om.robotics16@gmail.com>
2023-01-09 22:05:35 -0500

    Added some descriptions and two sections to homepg TODO: Figure out how to style and display information for the sections Get information for sections. Talk about that in the upcoming meeting this wednesday.   * (For that, figure out when we're meeting the commission)

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,17 @@ export default function Home() {
     <Layout title="Natick Commission on Disability" home>
       <main id={styles.main}>
         <hr />
-        <Section id="emergencyInfo" imgName="Emergency Information" title="Emergency Information" subtitle="Emergency Information">
+        <Section id="emergencyInfo" imgName="Emergency Information" title="Emergency Information" subtitle="Get guidance and support.">
           <br />
           <br />
+        </Section>
+        <br />
+        <Section id="whatIsNCOD" imgName="What is Commission on Disability" title="What is the Commission on Disability?" subtitle="A brief overview of the commission and its values.">
+          <br />
+          <br />
+        </Section>
+        <Section id="recentEvents" imgName="Recent Events" title="Recent and Upcoming Events" subtitle="Stay up to date with the latest events.">
+
         </Section>
       </main>
     </Layout>
