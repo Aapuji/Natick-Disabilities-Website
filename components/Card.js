6ec79ef9a293edd8ca6764e4ae8570aa8394e4ea
DevashishDas3 <dedas2024@gmail.com>
2023-01-29 22:17:51 -0500

    Cards - Structural Changes and Fixes (v1.1)

--- components/Card.js
+++ components/Card.js
@@ -2,9 +2,16 @@ import Link from "next/link";
 import Image from "next/image";
 import styles from '../styles/Card.module.css';
 
-const Card  = ({name, location, about}) => {
+const Card  = ({name, location, time, about}) => {
     return <>
-        <div className={styles.card}></div>
+        <div className={styles.card}>
+            <div className = {styles.cardcontent}>
+                <h1>{name}</h1>
+                <h4>Date and Time: {time}</h4>
+                <h4>Location: {location}</h4>
+                <p>About This Event: {about}</p>
+            </div>
+        </div>
     </>;
 }
 
--- pages/events.js
+++ pages/events.js
@@ -5,11 +5,51 @@ import Card from '../components/Card';
 export default function Events() {
   return <>
     <Layout title="Natick Commission on Disability">
-    <Card
-        name = "picnic"
-        location = "earth"
-        about = "lorem ipsum"
-      />
+    <h1 className={styles.upcomingHeading}>Ongoing and Upcoming Events</h1>
+    <div className={styles.cards}>
+      <Card
+          name = "NCOD Commission Meeting 3 hey my name is dev wow okay "
+          time = "January 31st | 9:30PM EST"
+          location = "NCOD Office"
+          about = "To all of those who are interested, this meeting will consost of brainstorming ideas for our events that are to be taking place this season. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis ultricies lacus sed turpis tincidunt id aliquet risus feugiat. Scelerisque eu ultrices vitae auctor eu. Elit pellentesque habitant morbi tristique senectus. Malesuada bibendum arcu vitae elementum curabitur vitae nunc sed velit. Tortor at auctor urna nunc id. Augue interdum velit euismod in pellentesque massa placerat duis ultricies. Tortor posuere ac ut consequat semper viverra nam libero justo. Pellentesque habitant morbi tristique senectus et. Egestas sed sed risus pretium quam vulputate. Mauris augue neque gravida in fermentum et sollicitudin ac orci. Sit amet mauris commodo quis imperdiet massa. Enim tortor at auctor urna nunc id. Lorem sed risus ultricies tristique nulla aliquet. Arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc. Quis lectus nulla at volutpat diam ut. Fermentum iaculis eu non diam phasellus vestibulum. Fusce id velit ut tortor pretium viverra suspendisse potenti nullam. Ut porttitor leo a diam sollicitudin tempor id. Luctus accumsan tortor posuere ac."
+        />
+    </div>
+    <div className={styles.cards}>
+      <Card
+          name = "picnic"
+          location = "earth"
+          about = "lorem ipsum"
+        />
+    </div>
+    <div className={styles.cards}>
+      <Card
+          name = "picnic"
+          location = "earth"
+          about = "lorem ipsum"
+        />
+    </div>
+    <h1 className={styles.pastHeading}>Notable Past Events</h1>
+    <div className={styles.cards}>
+      <Card
+          name = "picnic"
+          location = "earth"
+          about = "lorem ipsum"
+        />
+    </div>
+    <div className={styles.cards}>
+      <Card
+          name = "picnic"
+          location = "earth"
+          about = "lorem ipsum"
+        />
+    </div>
+    <div className={styles.cards}>
+      <Card
+          name = "picnic"
+          location = "earth"
+          about = "lorem ipsum"
+        />
+    </div>
     </Layout>
   </>;
 }
