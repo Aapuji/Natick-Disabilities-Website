38b91926d98013ee92135225a09896cfcaf0806c
DevashishDas3 <dedas2024@gmail.com>
2023-04-16 15:44:27 -0400

    Footing Work (Yellow Section)
    April 16

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,25 @@
 import Link from 'next/link';
 import styles from '../styles/Footer.module.css';
+import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <div className={styles.container}>
-    <footer className={styles.footerSection}>
-      <p>Created by Om Sharma & Devashish Das 2022</p>
+    <footer id={styles.footer} className={styles.footerContainer}>
+      <div id = {styles.footerNav} className = {styles.large}>
+            NAVIGATION
+            <div id = {styles.footerLinks} className={styles.footerContainer}>
+                <a href = "#" className = {styles.inline}>EMAIL</a>
+                <a href = "#" className = {styles.inline}>PHONE</a>
+                <a href = "#" className = {styles.inline}>LOCATION</a>
+                <br />
+            </div>
+      </div>
+      <div id={styles.footerSocials} className={styles.footerContainer}>
+            <a href="#" id={styles.twitterIcon}><Image src="" alt="twitter-icon" /></a>
+            <a href="#" id={styles.facebookIcon}><Image src="" alt="facebook-icon" /></a>
+            <a href="#" id={styles.instagramIcon}><Image src="" alt="instagram-icon" /></a>
+      </div>      
     </footer>
-    </div>
   );
 };
 
