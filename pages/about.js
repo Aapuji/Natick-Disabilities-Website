fc60dfc584567a1f2bffc7e2c91c3bf11c8f5f24
DevashishDas3 <dedas2024@gmail.com>
2023-04-25 18:37:21 -0400

    Profile Load Completion

--- pages/about.js
+++ pages/about.js
@@ -47,13 +47,43 @@ export default function About() {
             url = '/../public/default-profile.png'
           />
           <Profile
-            name = "Dev"
-            desc = "Dev and Om created this website"
+            name = "Elaine Ostroff (Vice Chairwoman)"
+            desc = "Elaine Ostroff has been involved with Disability Rights since the 1960s. She became aware of the discrimination toward persons with disabilities when she began working with the Massachusetts Department of Mental Health and Developmental Services. It was an extraordinary time, as there was a developing awareness that persons with a range of developmental and emotional difficulties had potential, and could interact and grow. She was part of the statewide effort to support and engage staff so that they might participate in the positive work with both clients as well as parents.Ostroff was the Founding Director of the Institute for Human Centered Design. Her professional papers are archived at the Smithsonian Museum of American History. She also was awarded an Honorary Doctorate from Middlebury College in 2016."
             url = '/../public/default-profile.png'
           />
           <Profile
-            name = "Dev"
-            desc = "Dev and Om created this website"
+            name = "Amanda Hsiao (Secretary)"
+            desc = "Amanda Hsiao joined the Natick Commission on Disability in September 2019 and was appointed as Secretary in January 2020. Amanda and her family moved to Natick in 2014. Amanda has a professional and personal interest in promoting equity and inclusivity for people with disabilities in the Natick community, and elsewhere."
+            url = '/../public/default-profile.png'
+          />
+          <Profile
+            name = "Aaron Spelker, CFA (Treasurer)"
+            desc = "Aaron Spelker joined the Natick Commission on Disability in September 2019 and was appointed as Treasurer in January 2020.  Aaron has lived in Natick for over 20 years and has 2 children in the Natick public school system.Aaron had a 20 year career in the investments and financial services industries concentrating on institutional investing, Aaron’s work focused on providing investment advice to corporate sponsored retirement plans, including pension plans, 401(k) plans and 403(b) plans. Aaron was a small business owner having founded Marathon Retirement & Pension Consulting (Marathon RPC) and later acted as the Director of Qualified Plans at Boston Wealth Management. Throughout his career, he offered strategic and tactical asset allocation advice to small and mid-sized institutional plan sponsors. Aaron is a CFA Charterholder and an active member of the Boston Security Analysts Society."
+            url = '/../public/default-profile.png'
+          />
+          <Profile
+            name = "Susan Peters (Commissioner)"
+            desc = "I am a graduate of the University of Maine with a BS in Elementary and Resident of Natick for 29 years. I serve, and have served, on my condo’s Board of Trustees for over 24 years. I serve on Encompass Hospital’s Patient &amp; Family Advisory Committee (Formally Braintree Rehab Hospital). I am also currently a Peer Mentor, at Encompass Rehab Hospital, for any patient who has suffered a spinal cord injury. I currently serve on Natick’s Council on Aging and Chair a subcommittee on Housing. I am serving on Natick’s Commission on Disability because I want to bring awareness to the Natick community that Natick cares about its residents with disabilities. I suffered a spinal cord injury 40+ years ago, before many opportunities were available to people with mobility issues. It is important for residents to know that we exist and are a valuable resource for programs, education, and accessibility issues."
+            url = '/../public/default-profile.png'
+          />
+          <Profile
+            name = "Jaslyne Agosto (Student Liaison)"
+            desc = "Jaslyne is the student liaison for the Natick Commission on Disability. She was a Natick resident four years ago but moved because of her military family and now she is back because of the good school systems. Now she goes to Natick High School where she participates in after school activities such as the Natick High Theatre program. She gives her insight when attending committee meetings and plans to be a part of the Commission for as long as possible."
+            url = '/../public/default-profile.png'
+          />
+          <Profile
+            name = "Sergeant Lacerra (Admin Supervisor)"
+            desc = "Sergeant Scott Lacerra has been with the Natick Police Department since October of 2005.  During his 18-year career he has been assigned to the Patrol Division and Traffic Division.  Sergeant Lacerra is currently assigned as an administrative supervisor.  His duties include special event planning, roadwork safety supervision, traffic safety, and liaison to the Natick Labs and the Commission on Disability.  Sergeant Lacerra is a firearms and use of force instructor for the department as well as a certified motorcycle operator. When not working he enjoys spending time with his wife, Chrissy, and three children, Joshua, Nicholas, and Isabella.  Sergeant Lacerra understands the issues that people with disabilities face on a personal level as his middle son Nicholas has Down Syndrome.  Through his experience with his own family he is able to better help the Commission on Disability in Natick by merging two things he is passionate about, the community he works for and the rights of the disabled. "
+            url = '/../public/default-profile.png'
+          />
+          <Profile
+            name = "Paul Carew (Commissioner)"
+            desc = "I started with the COD Board 20 years ago as an appointment.  13 years ago I was hired by the Town of Natick as Veterans Director,  one duty is ADA Coordinator.  My work now is with Veterans and their family members.  I am Director of a two person department for the town.  I am married to Peggy with one daughter Katie who is 24 years old.  I love my work with the Commission on Disability.  It is a great group of Natick Residents, helping others in need."
+            url = '/../public/default-profile.png'
+          />
+          <Profile
+            name = "Devashish Das & Om Sharma"
+            desc = "Our names are Devashish Das (Dev) and Om Sharma, and, at the time of the website's creation, we are juniors at the Natick High School. We are extremely grateful for the opportunity to create a website for the Natick Commission on Disability. Using our software engineering skills to make a positive impact in our community has been an incredible experience, and we are thankful for the chance to give back. Volunteering for this project was important to us because we wanted to contribute to the community and use our abilities to make a difference. We recognized the significance of having an accessible website that provides important information and resources to people with disabilities. Throughout the project, we remained dedicated to delivering a high-quality website that met the needs of the Natick Commission on Disability and the community it serves."
             url = '/../public/default-profile.png'
           />
         </div>
