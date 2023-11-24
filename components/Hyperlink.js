065d11d2d2c3d2bd9cf7d42cc2a2a1666139dc60
DevashishDas3 <dedas2024@gmail.com>
2023-11-24 15:49:48 -0500

    Past Meeting Work

--- components/Hyperlink.js
+++ components/Hyperlink.js
@@ -3,11 +3,10 @@ import Image from "next/image";
 import styles from '../styles/Hyperlink.module.css';
 import { Children } from "react";
 
-const Hyperlink = ({children, url, name, desc, descAsChild}) => {
+const Hyperlink = ({ url, name, children }) => {
     return <>
         <div className={styles.hyperlinkwhole}>
             <h1 className={styles.hyperlinkname}><Link href = {url}>{name}</Link></h1>
-            { descAsChild ? <></> : <p className={styles.hyperlinkdesc}>{desc}</p> }
             { children }
         </div>
     </>;
